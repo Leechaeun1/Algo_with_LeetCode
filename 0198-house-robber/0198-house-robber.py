143cc24510c709ae12233a0742f849524a546032
채은 <88484000+Leechaeun1@users.noreply.github.com>
2025-07-03 23:56:41 +0900

    Time: 0 ms (100%), Space: 12.5 MB (15.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,7 +2,7 @@ class Solution(object):
     def rob(self, nums):
         a=b=0
         for n in nums:
-            a,b=b,max(b,n+a)
+            a, b = b, max(n+a, b)
         return b
         """
         :type nums: List[int]
